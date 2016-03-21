60157939f7203c7ccd94469c39a54705574bc27f
Brian Sanchez <briansanchez@Nidias-MacBook-Pro.local>
2016-03-20 22:53:00 -0600

    added images to portfolio section. added "under construction sign"

--- includes/portfolio.php
+++ includes/portfolio.php
@@ -1,5 +1,6 @@
 <a name="portfolio"></a>
 <div class="portfolio">
+    <h2 style="text-align: center;text-decoration: underline; color: #A13F52;"><\under constuction></h2>
     <h1 class="sec-heading">Portfolio</h1>
     <!-- Projects --><?php include 'includes/projects.php'; ?>
     <!-- Report Card --><?php include 'includes/report-card.php'; ?>

efbdb54578cab2f98cd94fe6d75f0c271ea0da3f
Brian Sanchez <bsanchez11385@gmail.com>
2016-03-22 20:09:04 -0600

    added mailto to form

--- includes/form.php
+++ includes/form.php
@@ -1,17 +1,9 @@
 <div class="form-container">  
-  <form id="form" action="" method="post">
+  <form id="form" action="mailto:bsanchez11385@gmail.com?subject=Contact BackHat" enctype="text/plain" method="post">
     <h2 class="bottom-space-1">Fill this out!</h2>
-    <fieldset>
-      <input placeholder="Your name" type="text" tabindex="1" required autofocus>
-    </fieldset>
-    <fieldset>
-      <input placeholder="Your Email Address" type="email" tabindex="2" required>
-    </fieldset>
-    <fieldset>
-      <textarea placeholder="Type your Message Here...." tabindex="5" required></textarea>
-    </fieldset>
-    <fieldset>
-      <button name="submit" type="submit" class="sec-action-button" id="contact-submit" data-submit="...Sending">Submit</button>
-    </fieldset>
+      <input placeholder="Your name" type="text" name="name" tabindex="1" required autofocus>
+      <input placeholder="Your Email Address" type="email" name="email" tabindex="2" required>
+      <textarea placeholder="Type your Message Here...." name="message" tabindex="5" required></textarea>
+      <button type="submit" class="sec-action-button bottom-space-1" id="contact-submit" data-submit="...Sending">Submit</button>
   </form>
 </div>

ac46048b18840559e20d4f29846c92578cf0cb85
Brian Sanchez <bsanchez11385@gmail.com>
2016-03-23 15:22:51 -0600

    took out form autofocus

--- includes/form.php
+++ includes/form.php
@@ -1,7 +1,7 @@
 <div class="form-container">  
   <form id="form" action="mailto:bsanchez11385@gmail.com?subject=Contact BackHat" enctype="text/plain" method="post">
     <h2 class="bottom-space-1">Fill this out!</h2>
-      <input placeholder="Your name" type="text" name="name" tabindex="1" required autofocus>
+      <input placeholder="Your name" type="text" name="name" tabindex="1" required>
       <input placeholder="Your Email Address" type="email" name="email" tabindex="2" required>
       <textarea placeholder="Type your Message Here...." name="message" tabindex="5" required></textarea>
       <button type="submit" class="sec-action-button bottom-space-1" id="contact-submit" data-submit="...Sending">Submit</button>
